68de379f683cdaa474c8a6991143735a171e4482
onulee <onulee@naver.com>
2025-08-27 18:29:08 +0900

    member - 로그인,회원가입

--- s0827_01_jpa/src/main/java/com/site/controller/MemberController.java
+++ s0827_01_jpa/src/main/java/com/site/controller/MemberController.java
@@ -33,7 +33,7 @@ public class MemberController {
 	//ajax -> json데이터 전송
 	@ResponseBody
 	@PostMapping("/member/idBtn") // 중복id확인
-	public String idBtn(Member m) {
+	public Member idBtn(Member m) {
 		System.out.println("controller id : "+m.getId());
 		// findById(m.getId) -> service,serviceImpl,repository
 		Member member = memberService.findById(m.getId());
@@ -47,7 +47,10 @@ public class MemberController {
 			flag = "1";  // 아이디 사용가능
 		}
 		
-		return flag;
+		//Json방법 -> 직접 Json형태로 가공해서 방법
+		// 객체를 전송하면 자동으로 Json형태로 변환되어 전송
+		
+		return member;
 	}
 	
 	@GetMapping("/member/logout") //로그아웃
